a6e23c8af3ccd17e4bdf8764766fcebb24db429e
Afonso Ramon de Araujo Teles <afonso1907@hotmail.com>
2023-05-30 11:47:25 -0300

    VTEX course / Custom Blocks / Modifying the countdown block to have configurable styles

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,6 +1,7 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { TimeSplit } from './typings/global'
 import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
 
 interface CountdownProps {
   targetDate: string
@@ -8,20 +9,24 @@ interface CountdownProps {
 
 const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ 
+const CSS_HANDLES = ['countdown']
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   targetDate = DEFAULT_TARGET_DATE,
- }) => {
+}) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
-    seconds: '00'
+    seconds: '00',
   })
+
+  const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
   return (
-    <div>
-      <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
+    <div className={`${handles.countdown} c-muted-1 db tc bg-red`}>
+      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
     </div>
   )
 }
